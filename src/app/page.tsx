ac1e72cda6be46a9e67779198a4800124c0f33b1
MatheusPatriotaCIandT <mpatriota@ciandt.com>
2023-09-18 20:37:31 -0300

    feat: adding dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='bg-red-400 p-4'>Oi</div>
+      <div className='bg-red-400 p-4'>Login Page</div>
     </main>
   )
 }
