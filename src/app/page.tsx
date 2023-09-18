dcd0756749a4f76cbf142d2656e27c81480be32e
MatheusPatriotaCIandT <mpatriota@ciandt.com>
2023-09-18 20:51:32 -0300

    feat: adding phosphor react icons library to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+'use client'
+import { Cube } from '@phosphor-icons/react';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='bg-red-400 p-4'>Login Page</div>
+      <div className="bg-red-400 p-4 flex justify-center items-center gap-2 text-white">
+      <Cube color="teal" weight="duotone" />
+        Login Page</div>
     </main>
-  )
+  );
 }
