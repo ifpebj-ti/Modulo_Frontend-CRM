6bc9370f1efceaedee08164ba180d884b6ac7d3c
MatheusPatriota <mpatriota55@gmail.com>
2023-09-18 22:08:50 -0300

    feat: linking button with dashboard page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 import CavalcanteLogo from "../../public/logo.svg";
 import Input from "@/components/Input";
 import Button from "@/components/Button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <div className="flex flex-col items-center gap-6 rounded-3xl  pt-7 pb-7 pr-6 pl-6 border-gray-200 bg-white shadow-lg">
@@ -25,9 +28,7 @@ export default function Home() {
           </span>
         </div>
         <div>
-          <Button>
-            Entrar
-          </Button>
+          <Button onClick={()=> router.push("/dashboard")}>Entrar</Button>
         </div>
       </div>
     </div>
