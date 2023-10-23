d2589fd27af304c96bc11b9565e37738d9195aba
Eduardo Duarte (Kenopy) <xavier.duarte43@gmail.com>
2023-10-23 20:29:55 -0300

    Add Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer className="h-[100%] flex items-center justify-center bg-fc-color-300 text-fc-color-80 text-sm">
+            Copyright © 2023 - Farmácias Cavalcante - Todos os direitos reservados.
+        </footer>
+
+    )
+}
