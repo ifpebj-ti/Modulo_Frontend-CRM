dbab1cd2c054063ba1cee9aa450476b1b363e653
Everton <evertonlucaslg@gmail.com>
2023-12-12 10:19:08 -0300

    Fix: Base url marketing

--- src/services/MarketingApiSetup.ts
+++ src/services/MarketingApiSetup.ts
@@ -2,7 +2,7 @@ import { getSession } from "next-auth/react"
 import axios from "axios"
 
 const marketingApi = axios.create({
-	baseURL: "https://localhost:7094/api/",
+	baseURL: "http://150.136.54.24:8082/api/",
 })
 
 marketingApi.interceptors.request.use(
